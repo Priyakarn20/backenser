3cd1cf9180c4b3b360cd02f02014208c66e206a3
Priyakarn20 <priyakarn1997@gmail.com>
2022-10-18 11:02:30 +0530

    adding get data by id method

--- restapi/api/routes/faculty.js
+++ restapi/api/routes/faculty.js
@@ -3,14 +3,12 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
-        message:' It is related to get request of faculty',
+        message: ' It is related to get request of faculty',
     })
 })
 
 router.post('/', (req, res ,next) => {
-    res.status(200).json({
-        message:' It is related to post request of faculty',
-    })
+   console.log(req.body);
 })
 
 module.exports = router
--- restapi/api/routes/student.js
+++ restapi/api/routes/student.js
@@ -1,14 +1,60 @@
 const express = require('express');
 const router = express.Router();
+const Student = require('../../model/student.model');
+const mongoose = require('mongoose');
 
 router.get('/', (req,res,next)=>{
+   Student.find()
+   .then(result=>{
     res.status(200).json({
-        message:'This is student get request.',
+        studentData:result
+    });
+   })
+   .catch(err=>{
+console.log(err);
+res.status(500).json({
+    error:err
+   })
+});
+})
+
+router.get('/:id', (req, res,next)=>{
+    console.log(req.params.id);
+    Student.findById(req.params.id)
+    .then(result=>{
+    res.status(200).json({
+        student:result
     })
+})
+.catch(err=>{
+    console.log(err);
+    res.status(500).json({
+        error:err
+    })
+})
 })
 
 router.post('/',(req,res,next)=>{
-   console.log(req.body);
+   const student = new Student({
+    _id: new mongoose.Types.ObjectId,
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    age: req.body.age
+   })
+   student.save()
+   .then(result=>{
+    console.log(result);
+    res.status(200).json({
+        newStudent: result
+    })
+   })
+   .catch(err=>{
+    console.log(err);
+    res.status(500).json({
+        error:err
+    })
+   })
 })
 
 module.exports = router;
