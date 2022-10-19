78562aa478cad1cc472431864f82e1a3b4cbaef6
Priyakarn20 <priyakarn1997@gmail.com>
2022-10-19 15:36:13 +0530

    adding put and delete request

--- restapi1/api/routes/product.js
+++ restapi1/api/routes/product.js
@@ -4,6 +4,7 @@ const Product = require('../../model/model.product');
 const mongoose = require('mongoose');
 
 router.get('/', (req, res, next) => {
+
     Product.find()
         .then(result => {
             res.status(200).json({
@@ -56,5 +57,50 @@ router.post('/', (req, res, next) => {
             })
         })
 })
+
+//delete request
+router.delete('/:id', (req, res, next) => {
+    Product.remove({ _id: req.params.id })
+        .then(result => {
+            res.status(200).json({
+                message: 'Product deleted successfully',
+                result: result
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
+})
+
+//put request
+router.put('/:id', (req, res, next) => {
+    console.log(req.params.id);
+    Product.findOneAndUpdate({ _id: req.params.id }, {
+        $set: {
+            code: req.body.code,
+            title: req.body.title,
+            description: req.body.description,
+            mrp: req.body.mrp
+        }
+    })
+        .then(result => {
+            console.log(result);
+            res.status(200).json({
+                message: 'Product updated successfully',
+                result: result
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        })
+})
+
+
+
 
 module.exports = router
