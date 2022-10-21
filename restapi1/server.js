5823be4f9da380ea8f8237c3d07169dec4c08190
Priyakarn20 <priyakarn1997@gmail.com>
2022-10-22 00:16:37 +0530

    added some changes

--- restapi1/server.js
+++ restapi1/server.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const app = require('./app');
 const server = http.createServer(app);
-const port = process.env.Port || 4000;
+const port = process.env.Port ;
 server.listen(port, console.log('app is running on port'));
